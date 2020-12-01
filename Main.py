f4b25a059de3c928c0705d32298bf403ecc9ccc3
florianl64 <florianl64@unet.univie.ac.at>
2020-12-01 13:10:51 +0100

    solved day 1 part 2

--- Main.py
+++ Main.py
@@ -15,12 +15,24 @@ def day1part1(path):
     list1 = open_file_to_list(path)
     for left in list1:
         for walk in list1:
-
             if int(left) + int(walk) == 2020:
-                print(left + walk)
-                print(int(left) * int(walk))
                 return int(left) * int(walk)
+
+
+def day1part2(path):
+    """
+    They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+    :param path:
+    :return:
+    """
+    list1 = open_file_to_list(path)
+    for a in list1:
+        for b in list1:
+            for c in list1:
+                if int(a) + int(b) + int(c) == 2020:
+                    return int(a) * int(b) * int(c)
 
 
 if __name__ == "__main__":
     print('Day 1 part1 solution: ', day1part1('numbers'))
+    print('Day 1 part2 solution: ', day1part2('numbers'))
